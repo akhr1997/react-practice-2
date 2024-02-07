19787102b6cfef88902f55be36c86037f80012a6
akhr1997 <ak.hr1997@gmail.com>
2024-02-07 22:57:11 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+export const Header = () => {
+  return (
+    <header>
+      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {reactDescriptions[getRandomInt(2)]} React concepts you will need for
+        almost any app you are going to build! hehe
+      </p>
+    </header>
+  );
+};
